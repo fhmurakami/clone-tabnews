ec00c3f949277784e864c36974b6f3b6be23a1fe
Felipe Murakami <fhmurakami@gmail.com>
2023-10-14 20:45:02 +0000

    Adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+        <h1>Valeu, Filipe!</h1>
+        <img src="https://yt3.googleusercontent.com/ytc/APkrFKbqcDFU20JneEdewsgiLzveGvM998DJnv_uzVMr=s176-c-k-c0x00ffffff-no-rj" />
+        <p>Obrigado por toda a dedicação ao curso!</p>
+    </div>
+  )
+}
+
+export default Home;
