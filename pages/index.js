1a9aeca36b2257947638e57127853693c417f7b7
Felipe Murakami <fhmurakami@gmail.com>
2023-10-15 01:55:05 +0000

    Altera mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 function Home() {
   return (
     <div>
-        <h1>Valeu, Filipe!</h1>
-        <img src="https://yt3.googleusercontent.com/ytc/APkrFKbqcDFU20JneEdewsgiLzveGvM998DJnv_uzVMr=s176-c-k-c0x00ffffff-no-rj" />
-        <p>Obrigado por toda a dedicação ao curso!</p>
+        <h1>Oi, Gaby!</h1>
+        <p>Gostaria de dizer que estou realmente muito feliz pelo seu novo emprego! \o/</p>
+        <p>Sempre torci pelo seu sucesso, e continuarei torcendo. :)</p>
+        <p>Sinto falta do nosso convívio diário, das pausas pro cafézinho, das noites assistindo (Des)encanto em casa, ou jogando Miranha...</p>
+        <p>Sua companhia tornava o trampo muito mais leve.</p>
+        <p>Pena que acabamos nos afastando um pouco nesses últimos anos, mas saiba que eu continuo te achando foda pra caralho! 💜</p>
     </div>
   )
 }
