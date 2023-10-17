ebf3157061ee8bdab970b9369ce47cb55e9a55a5
Felipe Murakami <fhmurakami@gmail.com>
2023-10-17 20:14:09 +0000

    Adiciona `prettier` e scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Implementação do site TabNews (https://www.tabnews.com.br) para o curso.dev ministrado pelo @filipedeschamps (https://curso.dev) 
+
+Implementação do site TabNews (https://www.tabnews.com.br) para o curso.dev ministrado pelo @filipedeschamps (https://curso.dev)
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
 function Home() {
   return (
     <div>
-        <h1>Oi, Gaby!</h1>
-        <p>Gostaria de dizer que estou realmente muito feliz pelo seu novo emprego! \o/</p>
-        <p>Sempre torci pelo seu sucesso, e continuarei torcendo. :)</p>
-        <p>Sinto falta do nosso convívio diário, das pausas pro cafézinho, das noites assistindo (Des)encanto em casa, ou jogando Miranha...</p>
-        <p>Sua companhia tornava o trampo muito mais leve.</p>
-        <p>Pena que acabamos nos afastando um pouco nesses últimos anos, mas saiba que eu continuo te achando foda pra caralho! 💜</p>
+      <h1>Oi, Gaby!</h1>
+      <p>
+        Gostaria de dizer que estou realmente muito feliz pelo seu novo emprego!
+        \o/
+      </p>
+      <p>Sempre torci pelo seu sucesso, e continuarei torcendo. :)</p>
+      <p>
+        Sinto falta do nosso convívio diário, das pausas pro cafézinho, das
+        noites assistindo (Des)encanto em casa, ou jogando Miranha...
+      </p>
+      <p>Sua companhia tornava o trampo muito mais leve.</p>
+      <p>
+        Pena que acabamos nos afastando um pouco nesses últimos anos, mas saiba
+        que eu continuo te achando foda pra caralho! 💜
+      </p>
     </div>
-  )
+  );
 }
 
 export default Home;
